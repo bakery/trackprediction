efd7b2106159a24c2d3c983a1ea0eef4c6261d25
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2022-07-19 14:23:47 +0100

    app layout WIP

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,16 +2,31 @@ import * as React from "react";
 import {
   ChakraProvider,
   Box,
+  Container,
+  Flex,
+  HStack,
+  ButtonGroup,
+  Button,
+  IconButton,
+  Avatar,
+  Divider,
+  InputGroup,
+  InputLeftElement,
+  Icon,
+  Input,
+  Stack,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
+  useBreakpointValue,
+  useColorModeValue,
+  Grid,
 } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { FiHelpCircle, FiMenu, FiSearch, FiSettings } from "react-icons/fi";
+import { FaGithub, FaPaintBrush, FaTwitter } from "react-icons/fa";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "Logo";
 import { Predictions } from "./pages/Predictions";
 
 const client = new ApolloClient({
@@ -37,14 +52,87 @@ export const App = () => (
 );
 
 function Layout() {
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
   return (
-    <Box fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
+    <Box as="section" pb={{ base: "12", md: "24" }}>
+      <Box
+        as="nav"
+        bg="bg-surface"
+        boxShadow={useColorModeValue("sm", "sm-dark")}
+      >
+        <Container>
+          <Flex justify="space-between" py={{ base: "3", lg: "4" }}>
+            <HStack spacing="4">
+              <Logo />
+              {isDesktop && (
+                <ButtonGroup variant="ghost" spacing="1">
+                  <Button>Pending</Button>
+                  <Button aria-current="page">True</Button>
+                  <Button>False</Button>
+                  <ColorModeSwitcher justifySelf="flex-end" />
+                </ButtonGroup>
+              )}
+            </HStack>
+            {isDesktop ? null : (
+              <IconButton
+                variant="ghost"
+                icon={<FiMenu fontSize="1.25rem" />}
+                aria-label="Open Menu"
+              />
+            )}
+          </Flex>
+        </Container>
+      </Box>
+      <Container paddingTop="8">
+        <Grid minH="100vh">
           <Outlet />
-        </VStack>
-      </Grid>
+        </Grid>
+        <Divider pt="8" borderColor="bg-accent-subtle" />
+        <Stack
+          pt="8"
+          pb="12"
+          justify="space-between"
+          direction={{ base: "column-reverse", md: "row" }}
+          align="center"
+        >
+          <Text fontSize="sm" color="on-accent-subtle">
+            &copy; {new Date().getFullYear()} Prediction Tracker
+          </Text>
+          <ButtonGroup variant="ghost-on-accent">
+            <IconButton
+              as="a"
+              target={"_blank"}
+              href="https://excalidraw.com/#json=2tad7d02EJ_8iXLtcfkV7,JZZE5kCW3GI-IznW7U_Nig"
+              aria-label="Master plan"
+              icon={<FaPaintBrush fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              target={"_blank"}
+              href="https://github.com/bakery/trackprediction"
+              aria-label="GitHub"
+              icon={<FaGithub fontSize="1.25rem" />}
+            />
+            <IconButton
+              as="a"
+              target={"_blank"}
+              href="https://twitter.com/trackprediction"
+              aria-label="Twitter"
+              icon={<FaTwitter fontSize="1.25rem" />}
+            />
+          </ButtonGroup>
+        </Stack>
+      </Container>
     </Box>
   );
+  // return (
+  //   <Box fontSize="xl">
+  //     <Grid minH="100vh" p={3}>
+  //       <ColorModeSwitcher justifySelf="flex-end" />
+  //       <VStack spacing={8}>
+  //         <Outlet />
+  //       </VStack>
+  //     </Grid>
+  //   </Box>
+  // );
 }
